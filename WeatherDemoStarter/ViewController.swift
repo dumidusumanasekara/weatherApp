4dc75551af275f108992511cf6a55778ec079dcc
Dumidu <dumidusumanasekara@gmail.com>
2022-04-01 16:47:49 -0400

    Initial Commit

---
+++ WeatherDemoStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherDemoStarter
+//
+//  Created by Dumidu Sumanasekara on 2022-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
